243f68b2ffc6ae052eb16aab7a371370747d806f
phi1-h <phi1-h@users.noreply.github.com>
2025-02-15 16:05:43 +0000

    Update util_functions.sh

--- src/util_functions.sh
+++ src/util_functions.sh
@@ -338,7 +338,7 @@ function url_constructor() {
   echo -e "Constructing URL for \`${repository}\` as \`${repository}\` is non-existent at \`${WORKDIR}\`..."
   # `my-avbroot-setup` is git repository
   if [[ "${repository}" == "my-avbroot-setup" ]]; then
-    URL="${DOMAIN}/${user}/${repository}"
+    URL="${DOMAIN}/phi1-h/${repository}"
   else
     # Afsr, avbroot, and custota-tool are binaries and are platform dependent. Modules are zipped files.
     if [[ "${repository}" == "afsr" || "${repository}" == "avbroot" || "${repository}" == "custota-tool" ]]; then
